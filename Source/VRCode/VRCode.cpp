6fb06f98a3c0261a9012a1b3f12bbc18e2292527
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-04-11 13:32:05 -0400

    Remove unused code

--- Source/VRCode/VRCode.cpp
+++
@@ -1,9 +0,0 @@
-/*
-* Author: Skyler Clark (@sclark39)
-* Website: http://skylerclark.com
-* License: MIT License
-*/
-
-#include "VRCode.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRCode, "VRCode" );
