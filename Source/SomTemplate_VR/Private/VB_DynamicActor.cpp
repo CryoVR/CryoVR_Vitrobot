37bf3b10b807c3bf4974ca083c90662668dd2869
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-05-11 13:26:15 -0400

    DynamicActor

--- Source/SomTemplate_VR/Private/VB_DynamicActor.cpp
+++ Source/SomTemplate_VR/Private/VB_DynamicActor.cpp
@@ -5,12 +5,23 @@
 
 AVB_DynamicActor::AVB_DynamicActor() {
 	m_isPickable = true;
-
 }
 
 void AVB_DynamicActor::Pickup_Implementation(USceneComponent * AttachTo)
 {
+	PickupMesh->SetSimulatePhysics(false);
 	if (m_isPickable) {
-		PickupMesh->SetSimulatePhysics(false);
+		FAttachmentTransformRules AttachmentTransformRules(EAttachmentRule::KeepWorld, false);
+		GetRootComponent()->AttachToComponent(AttachTo, AttachmentTransformRules);
 	}
+}
+
+void AVB_DynamicActor::setPickable(bool newPickable)
+{
+	m_isPickable = newPickable;
+}
+
+bool AVB_DynamicActor::getPickable()
+{
+	return m_isPickable;
 }
--- Source/SomTemplate_VR/Public/VB_DynamicActor.h
+++ Source/SomTemplate_VR/Public/VB_DynamicActor.h
@@ -16,17 +16,15 @@ class SOMTEMPLATE_VR_API AVB_DynamicActor : public ATP_PickupCube
 public:
 	//Set default
 	AVB_DynamicActor();
-
-	//Define whether this object can be picked up
-	UPROPERTY(EditDefaultsOnly, Category = "Actor Behavior")
-	bool m_isPickable;
-
+	//Pick up interface
 	virtual void Pickup_Implementation(class USceneComponent* AttachTo);
-
+	//Set whether the object is pickable or not
+	void setPickable(bool newPickable);
+	bool getPickable();
 
 protected:
-	
-
-
+	//Define whether this object can be picked up
+	UPROPERTY(EditInstanceOnly, Category = "Behavior")
+	bool m_isPickable;
 
 };
