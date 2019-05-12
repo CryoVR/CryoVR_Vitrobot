1f0997fb80d466334b16969778bbe9080c51ba3d
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-05-12 17:51:48 -0400

    Airtank

---
+++ Source/SomTemplate_VR/Public/VB_AirTankActor.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2014-2019 Sombusta, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VB_StaticActor.h"
+#include "VB_AirTankActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOMTEMPLATE_VR_API AVB_AirTankActor : public AVB_StaticActor
+{
+	GENERATED_BODY()
+
+public:
+	AVB_AirTankActor();
+	
+};
