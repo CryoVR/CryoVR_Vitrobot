26c2db3357ed29d0d764fd169fb6672bfec115fe
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-05-12 17:37:15 -0400

    Revert "Revert "FloorDone""
    This reverts commit 369e7dfe027ca5cf8ac8c2d5721f9f23439da38a.

--- Source/SomTemplate_VR/Public/VB_FloorActor.h
+++ Source/SomTemplate_VR/Public/VB_FloorActor.h
@@ -20,7 +20,9 @@ public:
 	UFUNCTION()
 	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-protected:
-	FTimerHandle timeHandler;
+	UPROPERTY(EditAnywhere, Category = "Timer")
+	float teleportDynamicActorTimer;
 
+protected:
+	
 };
