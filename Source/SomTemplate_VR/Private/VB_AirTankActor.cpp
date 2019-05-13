494e927f013fd62632701cc903e14e6c535269b8
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-05-13 11:57:16 -0400

    ArtAssets

--- Source/SomTemplate_VR/Private/VB_AirTankActor.cpp
+++ Source/SomTemplate_VR/Private/VB_AirTankActor.cpp
@@ -1,6 +1,14 @@
 // Copyright (c) 2014-2019 Sombusta, All Rights Reserved.
 #include "VB_AirTankActor.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/StaticMeshComponent.h"
 
 AVB_AirTankActor::AVB_AirTankActor() {
+	ConstructorHelpers::FObjectFinder<UStaticMesh> SM_AirtankMesh(TEXT("StaticMesh'/Game/Models/EthaneTank.EthaneTank'"));
+	if (SM_AirtankMesh.Succeeded()) {
+		meshComp->SetStaticMesh(SM_AirtankMesh.Object);
+	}
+	
 
 }
+ 
