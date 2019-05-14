6dd70b45633c1fca20e5be0a305b5f5a0612aff4
Jiahui Dong <schubydong@gmail.com>
2019-05-14 18:07:07 -0400

    Cap

--- Source/SomTemplate_VR/Private/VB_DewarActor.cpp
+++ Source/SomTemplate_VR/Private/VB_DewarActor.cpp
@@ -5,12 +5,14 @@
 #include "Public/Engine.h"
 #include "Components/Boxcomponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "VB_NitrogenTankCapActor.h"
 
 AVB_DewarActor::AVB_DewarActor()
 {
 	BoxCompCap = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCompCap"));
 	BoxCompCap->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	BoxCompCap->OnComponentBeginOverlap.AddDynamic(this, &AVB_DewarActor::OnOverlapBegin);
+	BoxCompCap->OnComponentEndOverlap.AddDynamic(this, &AVB_DewarActor::OnOverlapEnd);
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Dewar(TEXT("StaticMesh'/Game/Models/DewarMesh.DewarMesh'"));
 	if (SM_Dewar.Succeeded())
@@ -22,12 +24,26 @@ AVB_DewarActor::AVB_DewarActor()
 	}
 }
 
+
 void AVB_DewarActor::PlayEffects()
 {
 	UGameplayStatics::SpawnEmitterAtLocation(this, FrozenFX, GetActorLocation());
 }
 
+
 void AVB_DewarActor::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	PlayEffects();
+	if (Cast<AVB_NitrogenTankCapActor>(OtherActor) != nullptr)
+	{
+		FrozenFX->SetVisibility(false);
+	}
+}
+
+
+void AVB_DewarActor::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (Cast<AVB_NitrogenTankCapActor>(OtherActor) != nullptr)
+	{
+		FrozenFX->SetVisibility(true);
+	}
 }
