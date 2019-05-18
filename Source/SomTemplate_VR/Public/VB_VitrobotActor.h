e964c7a9e601e69cca21ed48ea56a6707f7ccc52
zhangxiaomu01 <31782250+zhangxiaomu01@users.noreply.github.com>
2019-05-18 16:57:31 -0400

    Vitrobot button

--- Source/SomTemplate_VR/Public/VB_VitrobotActor.h
+++ Source/SomTemplate_VR/Public/VB_VitrobotActor.h
@@ -27,7 +27,7 @@ public:
 	UFUNCTION()
 	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	//virtual void Tick(float DeltaTime) override;
+	virtual void Tick(float DeltaTime) override;
 
 
 protected:
