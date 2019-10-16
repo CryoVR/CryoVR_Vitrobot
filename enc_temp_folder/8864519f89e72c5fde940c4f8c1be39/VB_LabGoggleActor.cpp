df1b28d8b46cb52eaea60a1ffc392e0d7f835c35
SchubyDong <>
2019-10-16 16:24:26 -0400

    safety

---
+++ enc_temp_folder/8864519f89e72c5fde940c4f8c1be39/VB_LabGoggleActor.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2014-2019 Sombusta, All Rights Reserved.
+
+
+#include "VB_LabGoggleActor.h"
+#include "Public/Engine.h"
+#include "Components/Boxcomponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "VB_LevelScriptActor.h"
+#include "VirtualReality/TP_MotionController.h"
+
+
+
+AVB_LabGoggleActor::AVB_LabGoggleActor()
+{
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Goggle(TEXT("StaticMesh'/Game/Models/DewarMesh.DewarMesh'"));
+	if (SM_Goggle.Succeeded())
+	{
+		UStaticMesh* Asset = SM_Goggle.Object;
+		PickupMesh->SetStaticMesh(Asset);
+		//BoxComp->SetupAttachment(PickupMesh);
+
+	}
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxComp->SetGenerateOverlapEvents(true);
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AVB_LabGoggleActor::OnOverlapBegin);
+}
+
+
+
+void AVB_LabGoggleActor::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	AVB_LevelScriptActor* LSA = Cast<AVB_LevelScriptActor>(GetWorld()->GetLevelScriptActor());
+	if (Cast<ATP_MotionController>(OtherActor) != nullptr)
+	{
+		//if (LSA != nullptr)
+		//{
+		//	if (LSA->GetStatus() == 0)
+		//	{
+		//		LSA->SetStatus(24);
+		//	}
+		//}
+		//Destroy();
+	}
+}
