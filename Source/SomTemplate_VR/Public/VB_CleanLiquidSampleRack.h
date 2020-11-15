29ab0b13b450ee7fb44475072bccf64f383ac8c9
SchubyDong <36869660+SchubyDong@users.noreply.github.com>
2020-11-15 14:14:59 -0500

    LiqudSampleRack Added

---
+++ Source/SomTemplate_VR/Public/VB_CleanLiquidSampleRack.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2017-2020 CryoVR, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VB_DynamicActor.h"
+#include "VB_CleanLiquidSampleRack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOMTEMPLATE_VR_API AVB_CleanLiquidSampleRack : public AVB_DynamicActor
+{
+	GENERATED_BODY()
+	
+};
